d85b980d6b77eca1911e539c75757c965528f972
yanghang <yanghang@pminfo.cn>
2021-10-14 20:07:39 +0800

    添加菜单

--- src/packages/admin/layout/components/menu/index.tsx
+++ src/packages/admin/layout/components/menu/index.tsx
@@ -1,11 +1,19 @@
-import {defineComponent} from 'vue'
+import {defineComponent, ref} from 'vue'
 import {defaultMenu} from '@/packages/admin/config/defaultMenu'
 export default defineComponent({
     name: 'yxs-menu-slider',
+    setup(){
+        const selectedKeys = ref<string[]>(['1']);
+        return{
+            selectedKeys
+        }
+    },
     render(ctx: any) {
         return (
             <div class="yxs-menu-slider">
-                111
+                <a-menu v-model:selectedKeys="ctx.selectedKeys" mode="inline" theme="light">
+
+                </a-menu>
             </div>
         )
     }
