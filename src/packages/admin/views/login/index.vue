e3042f5d64b8e90642f7842b83e50f040ee3a2a6
杨航 <hang.yang@jibaoyuan.net>
2021-10-19 23:48:04 +0800

    添加登录页面

--- src/packages/admin/views/login/index.vue
+++ src/packages/admin/views/login/index.vue
@@ -1,5 +1,78 @@
 <template>
-    <div>111</div>
+    <div class="login">
+        <div class="login-container">
+            <div class="login-content">
+                <div class="slide-left">1</div>
+                <div class="slide-right">
+                    <h2>欢迎您登录</h2>
+                    <p>你可以直接输入您的账号和密码登录</p>
+                    <div class="login-input">
+                        <i></i>
+                        <input type="text" placeholder="账户">
+                    </div>
+                    <div class="login-input">
+                        <i></i>
+                        <input type="text" placeholder="账户">
+                    </div>
+                    <div class="login-btn">
+                        <button>登录</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+    .login{
+        width: 100vw;
+        height: 100vh;
+        background-color: #ff9595;
+        &-container{
+            width: 75%;
+            height: 80%;
+            background-color: #ffffff;
+            border-radius: 40px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+        }
+        &-content{
+            display: flex;
+            width: 100%;
+            height: 100%;
+            padding: 100px;
+            box-sizing: border-box;
+            .slide-left{
+                flex: 1;
+            }
+            .slide-right{
+                flex: 1;
+                h2{
+                    font-size: 30px;
+                    color: #333333;
+                }
+                p{
+                    color: #d2d0d1;
+                    font-size: 14px;
+                }
+            }
+            .login-input{
+                margin-bottom: 15px;
+                input{
+                    background-color: #fff9f9;
+                    border-radius: 10px;
+                    padding: 10px;
+                    width: 80%;
+                    border: none;
+                }
+            }
+            .login-btn{
+                button{
+                    
+                }
+            }
+        }
+    }
+</style>
