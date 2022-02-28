c948df5692fc0ce0f171a38b082b8a1adf63944b
杨航 <hang.yang@jibaoyuan.net>
2022-02-28 23:14:29 +0800

    修复一个数据类型

--- egg/app/model/web_article.js
+++ egg/app/model/web_article.js
@@ -1,93 +1,93 @@
-const dayjs = require("dayjs");
+const dayjs = require('dayjs')
 module.exports = app => {
-    const {STRING, INTEGER, BOOLEAN, DATE,DataTypes} = app.Sequelize;
+    const { STRING, INTEGER, BOOLEAN, DATE, TEXT } = app.Sequelize
     return app.model.define('Web_Article', {
         id: {
             type: INTEGER,
             autoIncrement: true,
-            primaryKey: true
+            primaryKey: true,
         },
         user_id: {
             type: INTEGER,
-            comment: '用户id'
+            comment: '用户id',
         },
         channel_id: {
             type: INTEGER,
-            comment: '栏目id'
+            comment: '栏目id',
         },
         image: {
             type: STRING,
-            comment: '缩略图'
+            comment: '缩略图',
         },
         images: {
             type: STRING,
-            comment: '组图'
+            comment: '组图',
         },
         content: {
-            type: DataTypes.Text('long'),
-            comment: '内容'
+            type: TEXT('long'),
+            comment: '内容',
         },
         skip_url: {
             type: STRING,
-            comment: '跳转链接-当需要外部跳转链接时'
+            comment: '跳转链接-当需要外部跳转链接时',
         },
         description: {
             type: STRING,
-            comment: '描述'
+            comment: '描述',
         },
         views: {
             type: INTEGER,
-            comment: '浏览次数'
+            comment: '浏览次数',
         },
         likes: {
             type: INTEGER,
-            comment: '点赞次数'
+            comment: '点赞次数',
         },
         is_guest: {
             type: INTEGER,
-            comment: '是否游客访问'
+            comment: '是否游客访问',
         },
         is_comment: {
             type: INTEGER,
-            comment: '是否开启评论'
+            comment: '是否开启评论',
         },
         flag: {
             type: STRING,
-            comment: '置顶，最新，推荐，热门'
+            comment: '置顶，最新，推荐，热门',
         },
         dislikes: {
             type: INTEGER,
-            comment: '点踩'
+            comment: '点踩',
         },
         tags: {
             type: STRING,
-            comment: '标签'
+            comment: '标签',
         },
         title: {
             type: STRING,
-            comment: '权重'
+            comment: '权重',
         },
         title_style: {
             type: STRING,
-            comment: 'title样式'
+            comment: 'title样式',
         },
         createTime: {
             type: DATE,
             comment: '创建时间',
             get() {
-                return dayjs(this.getDataValue('createTime')).format('YYYY/MM/DD HH:mm:ss');
-            }
+                return dayjs(this.getDataValue('createTime')).format('YYYY/MM/DD HH:mm:ss')
+            },
         },
         updateTime: {
             type: DATE,
             comment: '更新时间',
             get() {
-                return dayjs(this.getDataValue('updateTime')).format('YYYY/MM/DD HH:mm:ss');
-            }
-        }
+                return dayjs(this.getDataValue('updateTime')).format('YYYY/MM/DD HH:mm:ss')
+            },
+        },
     }, {
         createdAt: 'createTime', // 指定名字
         updatedAt: 'updateTime',
-        tableName: 'yxs_web_article' // 定义实际表名
-    });
+        tableName: 'yxs_web_article', // 定义实际表名
+    })
 }
