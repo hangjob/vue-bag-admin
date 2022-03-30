e7e2542ba5042ff697cc329bc005dc6dc325ea3b
yanghang <yanghang@pminfo.cn>
2022-03-30 09:23:39 +0800

    删除lib-tuils文件

--- lib-utils/utils.es.js
+++
@@ -1,2 +0,0 @@
-const a = [{ a: 1 }];
-export { a as default };
--- lib-utils/utils.umd.js
+++
@@ -1,7 +0,0 @@
-(function(global, factory) {
-  typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global["web-utils"] = factory());
-})(this, function() {
-  "use strict";
-  const a = [{ a: 1 }];
-  return a;
-});
