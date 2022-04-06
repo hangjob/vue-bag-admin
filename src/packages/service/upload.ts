fc2a7b51210ecfb63d9dfb542d16dbfc7d643530
yanghang <yanghang@pminfo.cn>
2022-04-06 20:00:20 +0800

    优化servers

--- src/packages/service/upload.ts
+++ src/packages/service/upload.ts
@@ -1,7 +1,7 @@
 import { upload } from '@/packages/http/request'
 
 // 查询所有
-export const apiUploadImage = (params?: object) => {
+export const apiUploadImage = (params?: any) => {
     return upload(`/upload/uploadImg`, params)
 }
 
--- www/admin/service/channel.ts
+++ www/admin/service/channel.ts
@@ -1,37 +1,9 @@
-import { post } from '@/packages/http/request'
+import serviceBase from '@www/admin/service/serviceBase'
 
-const prefix = '/web/channel'
+const { apiAll, apiFind, apiCreate, apiUpdate, apiDelete, apiDeletes } = { ...serviceBase('/web/channel') }
 
-// 查询所有
-export const apiAll = (params?: object, config?: object) => {
-    return post(`${prefix}/all`, params, config)
+
+export {
+    apiAll, apiFind, apiCreate, apiUpdate, apiDelete, apiDeletes,
 }
 
-
-// 单个查询
-export const apiFind = (params?: object, config?: object) => {
-    return post(`${prefix}/find`, params, config)
-}
-
-
-// 添加
-export const apiCreate = (params?: object, config?: object) => {
-    return post(`${prefix}/create`, params, config)
-}
-
-
-// 编辑
-export const apiUpdate = (params?: object, config?: object) => {
-    return post(`${prefix}/update`, params, config)
-}
-
-
-// 删除
-export const apiDelete = (params?: object, config?: object) => {
-    return post(`${prefix}/delete`, params, config)
-}
-
-// 删除 多条
-export const apiDeletes = (params?: object, config?: object) => {
-    return post(`${prefix}/deletes`, params, config)
-}
