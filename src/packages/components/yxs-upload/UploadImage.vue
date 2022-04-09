44daa4e575b9f5b204080254698b231eed1c2c4e
杨航 <hang.yang@jibaoyuan.net>
2022-04-09 14:30:40 +0800

    上传图片美化

--- src/packages/components/yxs-upload/UploadImage.vue
+++ src/packages/components/yxs-upload/UploadImage.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="upload-image">
-        <a-modal v-model:visible="visible" width="1000px" okText="确认上传" cancelText="关闭"
-                 :cancelButtonProps="{danger: true,type: 'primary'}" title="上传图片" @ok="handleOk"
-                 :confirm-loading="confirmLoading"
+        <a-modal v-model:visible="tailor.visible" width="1000px" okText="确认上传" cancelText="关闭"
+                 :cancelButtonProps="{danger: true,type: 'primary'}" title="上传图片" @ok="tailor.handleConfirm"
+                 :confirm-loading="tailor.loading"
         >
             <div style="width: 100%;height: 500px">
                 <vueCropper
                     ref="cropper"
-                    :img="picture.base64"
+                    :img="tailor.base64"
                     :outputSize="0.8"
                     :autoCrop="true"
                     autoCropWidth="800"
@@ -38,37 +38,16 @@
     </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import {defineComponent, reactive, ref, watch} from 'vue'
 import 'vue-cropper/dist/index.css'
-import { VueCropper } from 'vue-cropper'
-import { apiUploadImage } from '@/packages/service/upload'
-import { message } from 'ant-design-vue'
+import {VueCropper} from 'vue-cropper'
+import {apiUploadImage} from '@/packages/service/upload'
+import {message} from 'ant-design-vue'
+import {imageConfig} from "@/packages/config";
+import base64ToFile from "__ROOT__/web-utils/file/base64ToFile";
+import fileToBase64 from "__ROOT__/web-utils/file/fileToBase64";
 
-interface FileItem {
-    uid: string;
-    name?: string;
-    status?: string;
-    response?: string;
-    url?: string;
-    size: number;
-}
 
-interface FileInfo {
-    file: FileItem;
-    fileList: FileItem[];
-}
-
-const dataURLtoFile = (data: any, fileName: string) => {
-    let arr = data.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        buster = atob(arr[1]),
-        n = buster.length,
-        u8arr = new Uint8Array(n)
-    while (n--) {
-        u8arr[n] = buster.charCodeAt(n)
-    }
-    return new File([u8arr], fileName, { type: mime })
-}
 export default defineComponent({
     components: {
         VueCropper,
@@ -79,48 +58,54 @@ export default defineComponent({
             default: '',
         },
     },
-    setup(props, { emit }) {
-        const visible = ref<boolean>(false)
-        const confirmLoading = ref<boolean>(false)
+    setup(props, {emit}) {
         const cropper = ref()
         const previewList = ref<any>([])
-        const picture = reactive({
-            change: () => {
-
-            },
+        const tailor = reactive({
+            visible: false,
+            loading: false,
             base64: '',
             fileName: '',
+            handleConfirm: () => {
+                tailor.loading = true
+                cropper.value.getCropData((base64: any) => {
+                    const file = base64ToFile(base64, tailor.fileName)
+                    apiUploadImage(file).then((data: any) => {
+                        const url = `${imageConfig.prefix}${data}`
+                        tailor.visible = false
+                        tailor.loading = false
+                        previewList.value.push({url, source: data})
+                        message.success('上传成功')
+                        emitImages()
+                    })
+                })
+            }
         })
+
+        watch(
+            () => props.image,
+            (newVal, oldVal) => {
+                if (newVal) {
+                    previewList.value = newVal?.split(',').map((item: any) => {
+                        return {url: `${imageConfig.prefix}${item}`, source: item}
+                    });
+                }
+            }
+        )
+
         const emitImages = () => {
-            const str = previewList.value.map(function(item: any) {
+            const str = previewList.value.map(function (item: any) {
                 return item.source
             }).join(',')
             emit('update:image', str)
         }
 
-        const handleOk = () => {
-            confirmLoading.value = true
-            cropper.value.getCropData((base64: any) => {
-                const file = dataURLtoFile(base64, picture.fileName)
-                apiUploadImage(file).then((data: any) => {
-                    const url = `${window.location.origin}/api${data}`
-                    visible.value = false
-                    confirmLoading.value = false
-                    previewList.value.push({ url, source: data })
-                    emitImages()
-                    message.success('上传成功')
-                })
-            })
-        }
-
         const beforeUpload = (file: any) => {
-            let reader = new FileReader()
-            reader.readAsDataURL(file)
-            picture.fileName = file.name
-            reader.onload = function(e: any) {
-                picture.base64 = e.target.result
-                visible.value = true
-            }
+            tailor.fileName = file.name
+            fileToBase64(file, ({base64}: { base64: any }) => {
+                tailor.base64 = base64
+                tailor.visible = true
+            })
         }
 
         const handleDeletePreview = (idx: number) => {
@@ -130,15 +115,12 @@ export default defineComponent({
 
 
         return {
-            visible,
-            picture,
-            handleOk,
             fileList: ref([]),
             beforeUpload,
-            confirmLoading,
             cropper,
             previewList,
             handleDeletePreview,
+            tailor
         }
     },
 })
