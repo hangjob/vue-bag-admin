26932edd057c1630dca07e5371cf90a0e5be69bd
杨航 <hang.yang@jibaoyuan.net>
2022-04-14 23:59:44 +0800

    过滤publish

--- src/packages/router/index.ts
+++ src/packages/router/index.ts
@@ -13,16 +13,16 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'admin',
-        component: () => import('@/packages/layout/index.vue'),
+        component: () => import('@/packages/layout/Index.vue'),
         children: [{ path: '', redirect: 'home' }],
     },
     {
         path: '/login', name: 'login', meta: { title: '登录' },
-        component: () => import('@/packages/views/login/index.vue'),
+        component: () => import('@/packages/views/login/Index.vue'),
     },
     {
         path: '/test', name: 'test', meta: { title: '测试页面' },
-        component: () => import('@/packages/views/test/index.vue'),
+        component: () => import('@/packages/views/test/Index.vue'),
     },
     {
         path: '/404',
