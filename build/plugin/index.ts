fbbf98d57157e3c7aaef3114132fbb6e4df10700
yanghang <yanghang@pminfo.cn>
2022-07-04 18:50:40 +0800

    增加体积优化查看

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -1,22 +1,24 @@
-import type {Plugin} from 'vite'
-import {vue} from './vue'
-import {vueJsx} from './vue-jsx'
-import {configStyleImport} from './style-import'
-import {configViteCompression} from "./compression";
-import {configHtml} from "./html";
-import {configViteComponents} from "./components";
-import {configWindiCSS} from "./windicss";
+import type { Plugin } from 'vite'
+import { vue } from './vue'
+import { vueJsx } from './vue-jsx'
+import { configStyleImport } from './style-import'
+import { configViteCompression } from './compression'
+import { configHtml } from './html'
+import { configViteComponents } from './components'
+import { configWindiCSS } from './windicss'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-export default function createVitePlugins({variables}: { variables: any }) {
-    const opt = {variables: variables}
+export default function createVitePlugins({ variables }: { variables: any }) {
+    const opt = { variables: variables }
     const vitePlugins: (Plugin | Plugin[])[] = [
         vue(),
         vueJsx(),
         configStyleImport(),
         configViteCompression(),
         configViteComponents(),
-        configWindiCSS()
-    ];
+        configWindiCSS(),
+        visualizer({  filename: 'report.html' }),
+    ]
     vitePlugins.push(configHtml(opt))
-    return vitePlugins;
+    return vitePlugins
 }
