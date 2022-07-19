9f9c1e8b33b2290fa888e859ae4d269d585bd1b0
杨航 <hang.yang@jibaoyuan.net>
2022-07-19 23:12:03 +0800

    权限组件

--- src/packages/views/sys/menu/Btns.vue
+++ src/packages/views/sys/menu/Btns.vue
@@ -2,13 +2,23 @@
     <a-drawer
         title="设置菜单页按钮"
         placement="right"
-        v-model:visible="visible"
-        width="40%"
+        v-model:visible="radio.visible"
+        width="25%"
         wrapClassName="auth"
     >
-
-        <div>
-            11
+        <div class="auth-body">
+            <a-select
+                v-model:value="radio.value"
+                mode="tags"
+                style="width: 100%"
+                placeholder="Tags Mode"
+                @change="handleChange"
+                @deselect="handleDeselect"
+            >
+                <a-select-option v-for="item in radio.items" :value="item.value" :label="item.name">
+                    {{ item.name }}
+                </a-select-option>
+            </a-select>
         </div>
         <div class="auth-action">
             <a-button type="primary" @click="handleSubmit">提交</a-button>
@@ -16,34 +26,46 @@
     </a-drawer>
 </template>
 <script lang="ts">
-import { computed, defineComponent, nextTick, ref, watch } from 'vue'
+import {computed, defineComponent, nextTick, reactive, ref, watch} from 'vue'
 
 export default defineComponent({
     setup(props, context) {
-
+        const radio = reactive({
+            items: [{name: '刷新', value: 'refresh', state: false}, {name: '新增', value: 'created', state: false},
+                {name: '删除', value: 'delete', state: false}, {name: '数据导出', value: 'export', state: false}
+            ],
+            value: [],
+            visible: false
+        })
         const handleSubmit = () => {
-
+            console.log(radio)
         }
-        const visible = ref(false)
+
+        const handleChange = (value) => {
+            radio.items.forEach((item) => {
+                item.state = !!(value.find((todo) => todo === item.value))
+            })
+        }
         const handleOpen = (show, record) => {
-            visible.value = true
+            radio.visible = true
+        }
+        const handleDeselect = (value) => {
+            console.log(value)
         }
         return {
             handleSubmit,
-            visible,
             handleOpen,
+            radio,
+            handleChange,
+            handleDeselect
         }
     },
 })
 </script>
-<style lang="less">
+<style lang="less" scoped>
 .auth {
-    .ant-drawer-body {
-        padding: 0 0 20px 0;
-    }
-
     &-body {
-        padding: 0 10px;
+        padding: 20px;
     }
 
     &-action {
