7597174a636684beef284acdd2aea4003a715d55
yanghang <yanghang@pminfo.cn>
2022-12-12 14:22:54 +0800

    升级plus

---
+++ src/packages/views/test/CurdPlus.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bag-curd">
+        <div class="bag-curd-container">
+            <div class="bag-curd-header">
+                <div class="bag-curd-action">
+                
+                </div>
+                <div class="bag-curd-prolate">
+                
+                </div>
+            </div>
+            <div class="bag-curd-table">
+            
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+
+</script>
---
+++ src/packages/views/test/Index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        1
+    </div>
+</template>
+<script lang="ts">
+
+</script>
