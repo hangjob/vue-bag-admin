f15cbb96d8c1306362652a0adc5203f294157290
yanghang <yanghang@pminfo.cn>
2023-02-21 14:03:11 +0800

    向下兼容

--- src/packages/views/test/Index.vue
+++ src/packages/views/test/Index.vue
@@ -1,19 +1,48 @@
 <template>
-    <bag-curd-plus :curdTable="curd.curdTable"></bag-curd-plus>
+<!--    <bag-curd-table :curdTable="curd.curdTable"></bag-curd-table>-->
+    <bag-curd-table :createForm="createForm" :editForm="editForm" :tableCurd="tableCurd"></bag-curd-table>
 </template>
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
 import columns from './columns'
 import initCurd, { createTableHock } from '@/packages/hook/tablePlus'
-
+import {cloneDeep} from 'lodash'
+import curdTableHock, { initTableHock } from '@/packages/hook/table'
 export default defineComponent({
-    components: {},
     setup() {
-        const defaultCurdTable = initCurd()
-        defaultCurdTable.apiPrefix = '/web/banner'
-        const curd = createTableHock({ columns, curdTable: defaultCurdTable })
+        const {tableCurd} = curdTableHock()
+        const form = reactive(initTableHock({
+            columns, tableCurd, options: {
+                apiPrefix: '/branch',
+            },
+        }))
+    
+        tableCurd.all.beforeSuccess = (res: any) => {
+            console.log(res)
+            tableCurd.pagination.total = 100
+            form.formItem.forEach((item) => {
+                if (item.formData.name === 'pid') {
+                    item.formData.options = res
+                }
+            })
+        }
+    
+        tableCurd.tableChange = (pagination, filters, sorter, { currentDataSource })=>{
+            tableCurd.pagination.current = pagination.current
+            tableCurd.pagination.pageSize = pagination.pageSize;
+            tableCurd.loading = true
+            tableCurd.all.handle()
+        }
+        
+        console.log(tableCurd)
+     
+        const editForm = cloneDeep(form)
+        const createForm = cloneDeep(form)
+    
         return {
-            curd,
+            tableCurd,
+            editForm,
+            createForm,
         }
     },
 })
